6e997530c61170a378f66b93fbb4c52ec1ca5470
Leon <leonnyongesa3@gmail.com>
2026-03-25 09:14:16 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+function calculateTax(amount) {
+    return amount * 0.1; 
+}
 
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
 
+function findMaximum(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+  } 
 
+function isPalindrome(word) {
+    const reversed = word.split('').reverse().join('');
+    return word === reversed;
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
+
+console.log(calculateTax(100)); // Should print 10
+console.log(convertToUpperCase("hello")); // Should print "HELLO"
+console.log(findMaximum(5, 10)); // Should print 10
+console.log(isPalindrome("racecar")); // Should print true
+console.log(calculateDiscountedPrice(100, 20)); // Should print 80
 
 
 // This is required for the test to function properly  
